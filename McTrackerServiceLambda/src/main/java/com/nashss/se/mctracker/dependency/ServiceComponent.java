ad263575280b8393f0713fa74bf893b3829dbeb4
Ryan Roberson <ryanroberson3@yahoo.com>
2023-11-14 14:37:01 -0600

    Get game log coming along

--- McTrackerServiceLambda/src/main/java/com/nashss/se/mctracker/dependency/ServiceComponent.java
+++ McTrackerServiceLambda/src/main/java/com/nashss/se/mctracker/dependency/ServiceComponent.java
@@ -12,36 +12,6 @@ public interface ServiceComponent {
      * Provides the relevant activity.
      * @return AddSongToPlaylistActivity
      */
-//    AddSongToPlaylistActivity provideAddSongToPlaylistActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return CreatePlaylistActivity
-//     */
-//    CreatePlaylistActivity provideCreatePlaylistActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return GetPlaylistActivity
-//     */
-//    GetPlaylistActivity provideGetPlaylistActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return GetPlaylistActivity
-//     */
-//    SearchPlaylistsActivity provideSearchPlaylistsActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return GetPlaylistSongsActivity
-//     */
-//    GetPlaylistSongsActivity provideGetPlaylistSongsActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return UpdatePlaylistActivity
-//     */
-//    UpdatePlaylistActivity provideUpdatePlaylistActivity();
+
 
 }
---
+++ McTrackerServiceLambda/src/main/java/com/nashss/se/mctracker/lambda/GetGameLogLambda.java
@@ -0,0 +1,35 @@
+package com.nashss.se.mctracker.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.mctracker.activity.requests.GetGameLogRequest;
+import com.nashss.se.mctracker.activity.results.GetGameLogResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GetGameLogLambda
+     extends LambdaActivityRunner<GetGameLogRequest, GetGameLogResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<GetGameLogRequest>, LambdaResponse> {
+
+        private final Logger log = LogManager.getLogger();
+
+        @Override
+        public LambdaResponse handleRequest (AuthenticatedLambdaRequest < GetGameLogRequest > input, Context context){
+            log.info("handleRequest from Get Game log LAMBDA");
+            return super.runActivity(
+                    () -> {
+                        GetGameLogRequest unauthenticatedRequest = input.fromPath(path ->
+                                GetGameLogRequest.builder()
+                                        .withGameId(path.get("gameId"))
+                                        .build());
+                        return input.fromUserClaims(claims ->
+                                GetGameLogRequest.builder()
+                                        .withGameId(unauthenticatedRequest.getGameId())
+                                        .withEmail(claims.get("email"))
+                                        .build());
+                    },
+                    (request, serviceComponent) ->
+                            serviceComponent.provideGetGameLogActivity().handleRequest(request)
+            );
+        }
+}
