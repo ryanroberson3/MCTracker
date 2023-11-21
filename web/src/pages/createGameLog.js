94e425abfa85c564b8afd6e74af8e3ae07c48ffa
Ryan Roberson <ryanroberson3@yahoo.com>
2023-11-21 14:49:25 -0600

    get player characters for dropdown is working

--- web/src/pages/createGameLog.js
+++ web/src/pages/createGameLog.js
@@ -8,13 +8,21 @@ class CreateGameLog extends BindingClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['mount', 'submit', 'redirectToViewGameLog'], this);
+        this.bindClassMethods(['clientLoaded', 'populateDropdown', 'mount', 'submit', 'redirectToViewGameLog'], this);
         this.dataStore = new DataStore();
         this.dataStore.addChangeListener(this.redirectToViewGameLog);
         this.header = new Header(this.dataStore);
     }
 
     async clientLoaded() {
+        const villains = await this.client.getCharactersByRole("Villain");
+        this.dataStore.set('villains', villains);
+
+        const heroes = await this.client.getCharactersByRole("Hero");
+        this.dataStore.set('heroes', heroes);
+
+        this.populateDropdown('villainDropdown', villains);
+        this.populateDropdown('heroDropdown', heroes);
     }
 
     mount() {
@@ -24,32 +32,60 @@ class CreateGameLog extends BindingClass {
         this.clientLoaded();
     }
 
+    async populateDropdown(dropdownId, characters) {
+        const dropdown = document.getElementById(dropdownId);
+        dropdown.innerHTML = "";
+
+        characters.forEach(character => {
+            const option = document.createElement('option');
+            option.value = character;
+            option.text = character;
+            dropdown.appendChild(option);
+        });
+    }
+
     async submit(evt) {
         evt.preventDefault();
-
+    
         const errorMessageDisplay = document.getElementById('error-message');
         errorMessageDisplay.innerText = '';
         errorMessageDisplay.classList.add('hidden');
-
+    
         const createButton = document.getElementById('create');
         const origButtonText = createButton.innerText;
         createButton.innerText = 'Creating...';
-
+    
         const date = document.getElementById('date').value;
         const aspectCheckboxes = document.querySelectorAll('input[name="aspect"]:checked');
         const aspect = Array.from(aspectCheckboxes).map(checkbox => checkbox.value);
-
+    
         const outcomeWL = document.getElementById('outcomeWL').value;
+    
+        // Updated: Use the selected value directly from the dropdowns
+        const villainDropdown = document.getElementById('villainDropdown');
+        const villain = villainDropdown.value;
+    
+        const heroDropdown = document.getElementById('heroDropdown');
+        const selectedHeroes = [];
+        const selectedOptions = heroDropdown.selectedOptions;
+        for (let i = 0; i < selectedOptions.length; i++) {
+            selectedHeroes.push(selectedOptions[i].value);
+        }
 
-        const heroesCheckboxes = document.querySelectorAll('input[name="heroes"]:checked');
-        const heroes = Array.from(heroesCheckboxes).map(checkbox => checkbox.value);
-        const villain = document.getElementById('villain').value;
-
-        const gameLog = await this.client.createGameLog(date, aspect, outcomeWL, villain, heroes, (error) => {
+        if (selectedHeroes.length > 4) {
+            // Handle the case where more than 4 heroes are selected (show an error, etc.)
+            createButton.innerText = origButtonText;
+            errorMessageDisplay.innerText = 'Error: You can select up to 4 heroes.';
+            errorMessageDisplay.classList.remove('hidden');
+            return;
+        }
+    
+        const gameLog = await this.client.createGameLog(date, aspect, outcomeWL, villain, selectedHeroes, (error) => {
             createButton.innerText = origButtonText;
             errorMessageDisplay.innerText = `Error: ${error.message}`;
             errorMessageDisplay.classList.remove('hidden');
         });
+    
         this.dataStore.set('gameLog', gameLog);
     }
 
